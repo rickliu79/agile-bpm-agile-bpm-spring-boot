83253d15cc8e2e451a26e6a532f4e8262f89f381
wacxhs <1429927559@qq.com>
2018-12-12 23:22:19 +0800

    调整调用

--- agilebpm-sys-autoconfigure/src/main/java/com/dstz/agilebpm/sys/autoconfiguration/AbMessageQueueAutoConfiguration.java
+++ agilebpm-sys-autoconfigure/src/main/java/com/dstz/agilebpm/sys/autoconfiguration/AbMessageQueueAutoConfiguration.java
@@ -3,8 +3,10 @@ package com.dstz.agilebpm.sys.autoconfiguration;
 import com.dstz.sys.api.jms.producer.JmsProducer;
 import com.dstz.sys.simplemq.consumer.CommonMessageQueueConsumer;
 import com.dstz.sys.simplemq.producer.CommonMessageQueueProducer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 import org.springframework.jms.annotation.EnableJms;
 
 /**
@@ -28,6 +30,13 @@ public class AbMessageQueueAutoConfiguration {
         return new CommonMessageQueueConsumer();
     }
 
+    @Bean
+    public MessageListenerAdapter commonMessageQueueConsumerListenerAdapter(@Autowired CommonMessageQueueConsumer messageQueueConsumer) {
+        MessageListenerAdapter messageListenerAdapter = new MessageListenerAdapter();
+        messageListenerAdapter.setDelegate(messageQueueConsumer);
+        messageListenerAdapter.setDefaultListenerMethod("handleMessage");
+        return messageListenerAdapter;
+    }
 
 
 }
--- agilebpm-sys-autoconfigure/src/main/java/com/dstz/agilebpm/sys/autoconfiguration/annotation/AbMessageQueueAutoConfigurationSelector.java
+++ agilebpm-sys-autoconfigure/src/main/java/com/dstz/agilebpm/sys/autoconfiguration/annotation/AbMessageQueueAutoConfigurationSelector.java
@@ -1,6 +1,7 @@
 package com.dstz.agilebpm.sys.autoconfiguration.annotation;
 
 import com.dstz.agilebpm.sys.autoconfiguration.AbMessageQueueAutoConfiguration;
+import com.dstz.agilebpm.sys.autoconfiguration.AbRedisMessageQueueAutoConfiguration;
 import com.dstz.agilebpm.sys.autoconfiguration.AbRedisMessageQueueConsumerProperties;
 import org.springframework.context.annotation.ImportSelector;
 import org.springframework.core.type.AnnotationMetadata;
@@ -17,9 +18,9 @@ public class AbMessageQueueAutoConfigurationSelector implements ImportSelector {
     @Override
     public String[] selectImports(AnnotationMetadata annotationMetadata) {
         Map<String, Object> attr = annotationMetadata.getAnnotationAttributes(EnableAbMessageQueue.class.getName());
-        MessageQueueType messageQueueType = MessageQueueType.valueOf((String) attr.get("value"));
+        MessageQueueType messageQueueType = (MessageQueueType)attr.get("value");
         if (MessageQueueType.REDIS.equals(messageQueueType)) {
-            return new String[]{AbRedisMessageQueueConsumerProperties.class.getName()};
+            return new String[]{AbRedisMessageQueueAutoConfiguration.class.getName()};
         } else if (MessageQueueType.JMS.equals(messageQueueType)) {
             return new String[]{AbMessageQueueAutoConfiguration.class.getName()};
         }
